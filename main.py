1131facc752227153273ce580f00d2a10fb13a30
Daryna Holikova <dgolikova0@gmail.com>
2023-09-23 21:48:23 +0100

    Experiment with turtle

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ table.add_column("Pokemon Name",["Pikachu","Squirtle","Charmander"])
 table.add_column("Type", ["Electric", "Water", "Fire"])
 
 table.align = 'l'
-# table.header_style = 'upper'
+table.header_style = 'upper'
 print(table)
 
 
