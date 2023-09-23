cc15a0b279bae4bc2b0a434d60911bbbe90ef595
Daryna Holikova <dgolikova0@gmail.com>
2023-09-23 11:30:09 +0100

    Experiment with turtle

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-# This is a sample Python script.
+from turtle import Turtle, Screen
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color('Coral')
+timmy.forward((100))
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-print('hello')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
